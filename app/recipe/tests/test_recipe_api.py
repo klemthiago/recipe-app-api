d4b61aeaa08cec58f5ca9352f2560cbb0be8c8e8
klemthiago <tpklem@gmail.com>
2025-04-11 08:13:35 -0300

    Starting configurations to create the Recipe API

---
+++ app/recipe/tests/test_recipe_api.py
@@ -0,0 +1,29 @@
+"""
+Test for recipe APIs
+"""
+from decimal import Decimal
+
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+
+from rest_framework.test import APIClient
+from rest_framework import status
+
+from core.models import Recipe
+
+from recipe.serializer import RecipeSerializer
+
+def create_recipe(user, **params):
+    """Create and return a sample recipe"""
+    defaults = {
+        'title': 'Sample recipe title',
+        'time_minutes': 22,
+        'price': Decimal('5.25'),
+        'description': 'Sample description',
+        'link': 'http://example.com/recipe.pdf'
+    }
+    defaults.update(params)
+
+    recipe = Recipe.objects.create(user=user, **defaults)
+    return recipe
