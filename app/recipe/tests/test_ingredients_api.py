9cae889deed6f449d1940917c57f77d406392b45
klemthiago <tpklem@gmail.com>
2025-05-03 15:36:00 -0300

    Finishing the Ingredients API

--- app/recipe/tests/test_ingredients_api.py
+++ app/recipe/tests/test_ingredients_api.py
@@ -89,16 +89,16 @@ class PrivateIngredientsApiTests(TestCase):
         ingredient.refresh_from_db()
         self.assertEqual(ingredient.name, payload['name'])
 
-    # def test_delete_ingredient(self):
-    #     """Test deleting an ingredient."""
-    #     ingredient = Ingredient.objects.create(user=self.user, name='Lettuce')
+    def test_delete_ingredient(self):
+        """Test deleting an ingredient"""
+        ingredient = Ingredient.objects.create(user=self.user, name='Lettuce')
 
-    #     url = detail_url(ingredient.id)
-    #     res = self.client.delete(url)
+        url = detail_url(ingredient.id)
+        res = self.client.delete(url)
 
-    #     self.assertEqual(res.status_code, status.HTTP_204_NO_CONTENT)
-    #     ingredients = Ingredient.objects.filter(user=self.user)
-    #     self.assertFalse(ingredients.exists())
+        self.assertEqual(res.status_code, status.HTTP_204_NO_CONTENT)
+        ingredients = Ingredient.objects.filter(user=self.user)
+        self.assertFalse(ingredients.exists())
 
     # def test_filter_ingredients_assigned_to_recipes(self):
     #     """Test listing ingedients to those assigned to recipes."""
